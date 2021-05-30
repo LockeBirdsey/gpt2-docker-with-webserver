fc17a824ed92ab1934a153c3c0c5666aef3c98ad
Locke Birdsey <locke.birdsey@gmail.com>
2021-05-30 12:01:35 +0300

    update dockerfile and server code for testing

--- Dockerfile
+++ Dockerfile
@@ -13,10 +13,9 @@ WORKDIR gpt2-docker-with-webserver-master
 RUN chmod +x entrypoint.sh
 RUN pip3 install -r requirements.txt
 RUN pip3 install gunicorn
-#TODO: Should we download basic 124M model on build?
+#TODO: Should we download model on build?
 RUN python startup.py
 
 #Get the things running
 ENTRYPOINT ["/gpt2-docker-with-webserver-master/entrypoint.sh"]
 CMD ["run"]
-#CMD ["gunicorn"  , "--bind", "0.0.0.0:8000", "server:app"]
--- server.py
+++ server.py
@@ -3,12 +3,20 @@ from pathlib import Path
 
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-
 from gpt2_process import GPT2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = "./upload"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+NO_FILE = "NOFILE"
+app.config['CORPUS_FILE_NAME'] = NO_FILE
+
+
+@app.route('/')
+def index(file_name=NO_FILE, results=None):
+    if file_name != NO_FILE:
+        app.config['CORPUS_FILE_NAME'] = file_name
+    return render_template("index.html", file_name=app.config['CORPUS_FILE_NAME'], results=results)
 
 
 @app.route('/upload')
@@ -22,17 +30,26 @@ def upload_file():
         f = request.files['file']
         f_name = secure_filename(f.filename)
         f.save(f_name)
-        # This is where we call gpt2
-        gpt2 = GPT2()
-        model_name = os.environ['GPT2_MODEL_NAME']
-        file_name = str(Path(app.config['UPLOAD_FOLDER']).joinpath(f_name))
-        sess = gpt2.finetune(file_name=file_name, model_name=model_name)
-        gen = []
-        for i in range(20):
-            gen.append(gpt2.generate_text(sess))
-        print(gen)
-        return '\n'.join(gen)
+        file_name = str(f_name)
+        print(file_name)
+        return index(file_name=file_name)
         # return 'file uploaded successfully and stored at {}'.format(f.filename)
+
+
+@app.route('/run_process')
+def start_process():
+    f_name = app.config['CORPUS_FILE_NAME']
+    if f_name == NO_FILE:
+        return index()
+    gpt2 = GPT2()
+    model_name = os.environ['GPT2_MODEL_NAME']
+    file_name = str(Path(app.config['UPLOAD_FOLDER']).joinpath(f_name))
+    sess = gpt2.finetune(file_name=file_name, model_name=model_name)
+    gen = []
+    for i in range(20):
+        gen.append(gpt2.generate_text(sess))
+    print(gen)
+    return ""
 
 
 if __name__ == '__main__':
